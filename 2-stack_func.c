86c211a08778dd95eba05cdcdba1e8d420aef2e6
Suitret <suitret01@gmail.com>
2023-05-26 00:09:26 +0100

    7. div

--- 2-stack_func.c
+++ 2-stack_func.c
@@ -60,3 +60,27 @@ void sub_val(stack_t **head, unsigned int index)
 	else
 		error_with_line(": can't sub, stack too short", index);
 }
+/**
+ * div_val - adds a new node at the beginning of a stack_t list
+ * @head:  pointer to the head of list
+ * @index: number
+ * Return: the head of list
+ */
+void sub_val(stack_t **head, unsigned int index)
+{
+	int temp;
+
+	if (*head && ((*head)->next))
+	{
+		if ((*head)->n != 0)
+		{
+			temp = ((*head)->next->n) / ((*head)->n);
+			(*head)->next->n = temp;
+			pop_top_node(head, index);
+		}
+		else
+			error_with_line(": division by zero", index);	
+	}
+	else
+		error_with_line(": can't div, stack too short", index);
+}
